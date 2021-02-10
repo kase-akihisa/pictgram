699bc79392a236a62094cc4def6dc4582fad9b6d
kase-akihisa <kase.of.1995@outlook.jp>
2021-02-10 08:08:12 +0000

    rails 04

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, length: { maximum: 300 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{8,32}\z/
+   validates :password, presence: true,
+            format: { with: VALID_PASSWORD_REGEX,
+             message: "は半角8~32文字英大文字・小文字・数字それぞれ１文字以上含む必要があります"}
+  
+  has_secure_password
+end
