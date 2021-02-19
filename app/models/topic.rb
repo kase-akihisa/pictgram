37b33dd70e11400110f61e3e0f6ffeb2d11197d5
kase-akihisa <kase.of.1995@outlook.jp>
2021-02-19 06:05:13 +0000

    son

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,6 @@ class Topic < ApplicationRecord
   validates :user_id, presence: true
   validates :description, presence: true
   validates :image, presence: true
-  validates :text, presence: true
   
   belongs_to :user
   
